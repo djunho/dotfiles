5daf417d3158932f0ffd960962ac5b9ab3f5abaa
Daniel Junho <djunho@gmail.com>
2019-11-27 14:58:17 -0300

    Sets command to run the plugin manager
    Change-Id: I66d971708a7a8def1a3537f3145892fe8f7c461a

--- tmux/install.sh
+++ tmux/install.sh
@@ -2,6 +2,10 @@
 
 cp tmux.conf $HOME/.tmux.conf
 
+# Install the tmux plugin manager
+echo "Install tmux plugin manager"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 if [ -z "$1" ];  then
     echo -e "Copying the tmux file."
 else
