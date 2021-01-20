d5024ccaf063ec95ca33ae334cfe1addada51b8a
Daniel Junho <djunho@gmail.com>
2021-01-20 09:46:22 +0000

    fish: Add key binds f9 and f12

---
+++ fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,5 @@
+function fish_user_key_bindings
+    bind -k f9 "git status; commandline -f repaint"
+    bind -k f12 "echo -ne '\ec\e[3J'; commandline -f repaint"
+    bind \cc 'commandline ""'
+end
