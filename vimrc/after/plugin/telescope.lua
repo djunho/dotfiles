d2e9ad3117e7dc1d8ae4be9a061ea2e117ce116e
Daniel Junho <djunho@gmail.com>
2023-04-18 16:52:54 +0100

    [vim] Update telescope settings

--- vimrc/after/plugin/telescope.lua
+++ vimrc/after/plugin/telescope.lua
@@ -2,7 +2,52 @@
 -- See `:help telescope` and `:help telescope.setup()`
 require('telescope').setup {
     defaults = {
+        vimgrep_arguments = {
+            "rg",
+            "-L",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+        },
+        prompt_prefix = "   ",
+        selection_caret = "  ",
+        entry_prefix = "  ",
+        initial_mode = "insert",
+        selection_strategy = "reset",
+        sorting_strategy = "ascending",
+        layout_strategy = "horizontal",
+        layout_config = {
+            horizontal = {
+                prompt_position = "top",
+                preview_width = 0.55,
+                results_width = 0.8,
+            },
+            vertical = {
+                mirror = false,
+            },
+            width = 0.87,
+            height = 0.80,
+            preview_cutoff = 120,
+        },
+        file_sorter = require("telescope.sorters").get_fuzzy_file,
+        file_ignore_patterns = { "node_modules" },
+        generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+        path_display = { "truncate" },
+        winblend = 0,
+        border = {},
+        borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+        color_devicons = true,
+        set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+        file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+        grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+        qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+        -- Developer configurations: Not meant for general override
+        buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
         mappings = {
+            n = { ["q"] = require("telescope.actions").close },
             i = {
                 ['<C-u>'] = false,
                 ['<C-d>'] = false,
@@ -26,6 +71,7 @@ vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently o
 
 vim.keymap.set('n', '<C-f>',     builtin.current_buffer_fuzzy_find, { desc = '[/] Fuzzily search in current buffer]' })
 vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, { desc = '[/] Fuzzily search in current buffer]' })
+vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch in all [B]uffers' })
 
 vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<C-g>',      builtin.live_grep, { desc = '[S]earch by [G]rep' })
