029988cc29b55e0e6c000d73eaa6576899f97de1
Daniel Junho <djunho@gmail.com>
2023-07-20 14:58:28 +0100

    Add new languages to the treesitter

--- vimrc/after/plugin/treesitter.lua
+++ vimrc/after/plugin/treesitter.lua
@@ -2,7 +2,7 @@
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'lua', 'python', 'bash', 'vim', 'help' },
+    ensure_installed = { 'c', 'cpp', 'lua', 'python', 'bash', 'vim', 'help', 'markdown', 'markdown_inline', 'yaml', 'json', 'regex' },
 
     highlight = { enable = true },
     indent = { enable = true, disable = { 'python' } },
