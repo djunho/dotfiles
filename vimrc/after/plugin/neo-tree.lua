014c35b79641b7be20d64f73b222d79d504dedf1
Daniel Junho <djunho@gmail.com>
2023-09-22 14:26:53 +0100

    Update Neo-tree and bufferline

--- vimrc/after/plugin/neo-tree.lua
+++ vimrc/after/plugin/neo-tree.lua
@@ -19,7 +19,7 @@ require("neo-tree").setup({
     enable_diagnostics = false,
     open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
     sort_case_insensitive = false, -- used when sorting files and directories in the tree
-    sort_function = nil , -- use a custom function for sorting files and directories in the tree 
+    sort_function = nil , -- use a custom function for sorting files and directories in the tree
     -- sort_function = function (a,b)
     --             if a.type == b.type then
     --                     return a.path > b.path
@@ -66,16 +66,16 @@ require("neo-tree").setup({
         git_status = {
             symbols = {
                 -- Change type
-                added         = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
-                modified    = "", -- or "", but this is redundant info if you use git_status_colors on the name
-                deleted     = "✖",-- this can only be used in the git_status source
-                renamed     = "",-- this can only be used in the git_status source
+                added     = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+                modified  = "", -- or "", but this is redundant info if you use git_status_colors on the name
+                deleted   = "✖",-- this can only be used in the git_status source
+                renamed   = "",-- this can only be used in the git_status source
                 -- Status type
                 untracked = "",
-                ignored     = "",
-                unstaged    = "",
-                staged        = "",
-                conflict    = "",
+                ignored   = "",
+                unstaged  = "",
+                staged    = "",
+                conflict  = "",
             }
         },
     },
@@ -92,9 +92,9 @@ require("neo-tree").setup({
             nowait = true,
         },
         mappings = {
-            ["<space>"] = { 
-                    "toggle_node", 
-                    nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use 
+            ["<space>"] = {
+                    "toggle_node",
+                    nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
             },
             ["<2-LeftMouse>"] = "open",
             ["<cr>"] = "open",
@@ -114,7 +114,7 @@ require("neo-tree").setup({
             -- ['C'] = 'close_all_subnodes',
             ["z"] = "close_all_nodes",
             --["Z"] = "expand_all_nodes",
-            ["a"] = { 
+            ["a"] = {
                 "add",
                 -- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
                 -- some commands may take optional config options, see `:h neo-tree-mappings` for details
@@ -168,14 +168,17 @@ require("neo-tree").setup({
                 --".null-ls_*",
             },
         },
-        follow_current_file = false, -- This will find and focus the file in the active buffer every
-                                     -- time the current file is changed while the tree is open.
+        follow_current_file = {
+            enabled = false, -- This will find and focus the file in the active buffer every time
+            --               -- the current file is changed while the tree is open.
+            leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+        },
         group_empty_dirs = false, -- when true, empty folders will be grouped together
         hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
                                                 -- in whatever position is specified in window.position
                                                 -- "disabled",  -- netrw left alone, neo-tree does not handle opening dirs
         use_libuv_file_watcher = true, -- This will use the OS level file watchers to detect changes
-                                                                        -- instead of relying on nvim autocmd events.
+                                       -- instead of relying on nvim autocmd events.
         window = {
             mappings = {
                 ["<bs>"] = "navigate_up",
@@ -185,31 +188,34 @@ require("neo-tree").setup({
                 ["D"] = "fuzzy_finder_directory",
                 ["#"] = "fuzzy_sorter", -- fuzzy sorting using the fzy algorithm
                 -- ["D"] = "fuzzy_sorter_directory",
-                ["f"] = "filter_on_submit",
+                ["f"]     = "filter_on_submit",
                 ["<c-x>"] = "clear_filter",
-                ["[g"] = "prev_git_modified",
-                ["]g"] = "next_git_modified",
+                ["[g"]    = "prev_git_modified",
+                ["]g"]    = "next_git_modified",
             },
             fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
                 ["<down>"] = "move_cursor_down",
-                ["<C-n>"] = "move_cursor_down",
-                ["<up>"] = "move_cursor_up",
-                ["<C-p>"] = "move_cursor_up",
+                ["<C-n>"]  = "move_cursor_down",
+                ["<up>"]   = "move_cursor_up",
+                ["<C-p>"]  = "move_cursor_up",
             },
         },
 
         commands = {} -- Add a custom command or override a global one using the same function name
     },
     buffers = {
-        follow_current_file = false, -- This will find and focus the file in the active buffer every
-                                     -- time the current file is changed while the tree is open.
+        follow_current_file = {
+            enabled = false, -- This will find and focus the file in the active buffer every time
+            --               -- the current file is changed while the tree is open.
+            leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+        },
         group_empty_dirs = true, -- when true, empty folders will be grouped together
         show_unloaded = true,
         window = {
             mappings = {
-                ["bd"] = "buffer_delete",
+                ["bd"]   = "buffer_delete",
                 ["<bs>"] = "navigate_up",
-                ["."] = "set_root",
+                ["."]    = "set_root",
             }
         },
     },
@@ -217,7 +223,7 @@ require("neo-tree").setup({
         window = {
             position = "float",
             mappings = {
-                ["A"]    = "git_add_all",
+                ["A"]  = "git_add_all",
                 ["gu"] = "git_unstage_file",
                 ["ga"] = "git_add_file",
                 ["gr"] = "git_revert_file",
--- vimrc/init.lua
+++ vimrc/init.lua
@@ -55,7 +55,7 @@ require('packer').startup(function(use)
 
     use {
         'nvim-neo-tree/neo-tree.nvim',
-        branch = 'v2.x',
+        branch = 'v3.x',
         requires = {
             'nvim-lua/plenary.nvim',
             'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
@@ -63,7 +63,7 @@ require('packer').startup(function(use)
         }
     }
 
-    use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons'}
+    use {'akinsho/bufferline.nvim', tag = "v4.*", requires = 'nvim-tree/nvim-web-devicons'}
 
     -- Git related plugins
     use 'tpope/vim-fugitive'
