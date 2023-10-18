f499e6edb5f8aae6ebdcb863420cb88ae69053d6
Daniel Junho <djunho@gmail.com>
2023-10-18 14:56:42 +0100

    [LSP] Create a keymap to copy the compile_commands to root folder

--- vimrc/after/plugin/nvim-lspconfig.lua
+++ vimrc/after/plugin/nvim-lspconfig.lua
@@ -120,3 +120,60 @@ mason_lspconfig.setup_handlers {
     end,
 }
 
+local actions = require "telescope.actions"
+local actions_state = require "telescope.actions.state"
+local pickers = require "telescope.pickers"
+local finders = require "telescope.finders"
+local sorters = require "telescope.sorters"
+local dropdown = require "telescope.themes".get_dropdown()
+
+function dump(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+end
+
+-- local colors = vim.fn.getcompletion("", "color")
+local cmd = { "fd", "-uu", "--type", "f", "--color", "never", "compile_commands.json" }
+
+local function enter(prompt_bufnr)
+    local selected = actions_state.get_selected_entry()
+    vim.cmd("!/bin/rm compile_commands.json")
+    vim.cmd("!ln -s "..selected[1].." .")
+    vim.cmd("LspRestart")
+    actions.close(prompt_bufnr)
+end
+
+local function next_color(prompt_bufnr)
+    actions.move_selection_next(prompt_bufnr)
+end
+
+local function prev_color(prompt_bufnr)
+    actions.move_selection_previous(prompt_bufnr)
+end
+
+local opts = {
+    finder = finders.new_oneshot_job(cmd, {}),
+    sorter = sorters.get_generic_fuzzy_sorter({}),
+
+    attach_mappings = function(prompt_bufnr, map)
+        map("i", "<CR>", enter)
+        map("i", "<C-j>", next_color)
+        map("i", "<C-k>", prev_color)
+        map("n", "<CR>", enter)
+        map("n", "<C-j>", next_color)
+        map("n", "<C-k>", prev_color)
+        return true
+    end,
+
+}
+
+vim.keymap.set("n", "<leader>lc", function() pickers.new(dropdown, opts):find() end, { desc = "[L]sp [C]onfig" } )
+
